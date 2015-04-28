db9a883f879d16d2f418f25ac29f9a008316ff39
zhangxiaoliang <toadgg@gmail.com>
2015-04-28 19:22:49 +0800

    chapter 2 finish

--- src/main/java/com/avicui/studentinfo/CourseSession.java
+++ src/main/java/com/avicui/studentinfo/CourseSession.java
@@ -1,6 +1,9 @@
 package com.avicui.studentinfo;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by zhangxiaoliang on 15/4/28.
@@ -10,10 +13,17 @@ public class CourseSession {
     private String department;
     private String number;
     private ArrayList<Student> students = new ArrayList<Student>();
+    private Date startDate;
 
     public CourseSession(String department, String number) {
         this.department = department;
         this.number = number;
+    }
+
+    public CourseSession(String department, String number, Date startDate) {
+        this.department = department;
+        this.number = number;
+        this.startDate = startDate;
     }
 
     public String getDepartment() {
@@ -32,7 +42,20 @@ public class CourseSession {
         students.add(student);
     }
 
-    public ArrayList<Student> getAllStudents() {
-        return students;
+    public Student get(int index) {
+        return students.get(index);
+    }
+
+    public Date getEndDate() {
+
+        final int sessionLength = 16;
+        final int daysInWeek = 7;
+        final int daysFromFridayToMonday = 3;
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(startDate);
+        int numberOfDays = sessionLength * daysInWeek - daysFromFridayToMonday;
+        calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+        return calendar.getTime();
     }
 }
--- src/test/java/com/avicui/studentinfo/CourseSessionTest.java
+++ src/test/java/com/avicui/studentinfo/CourseSessionTest.java
@@ -1,8 +1,11 @@
 package com.avicui.studentinfo;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -12,9 +15,27 @@ import static org.junit.Assert.assertThat;
  */
 public class CourseSessionTest {
 
+    private CourseSession session;
+    private Date startDate;
+
+    @Before
+    public void setUp() throws Exception {
+        startDate = createDate(2003, 1, 6);
+        session = new CourseSession("ENGL", "101");
+    }
+
+    private Date createDate(int year, int month, int date) {
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.DATE, date);
+        return calendar.getTime();
+    }
+
     @Test
     public void testCreate() throws Exception {
-        CourseSession session = new CourseSession("ENGL", "101");
+
         assertThat(session.getDepartment(), is("ENGL"));
         assertThat(session.getNumber(), is("101"));
         assertThat(session.getNumberOfStudents(), is(0));
@@ -22,21 +43,23 @@ public class CourseSessionTest {
 
     @Test
     public void testEnrollStudents() throws Exception {
-        CourseSession session = new CourseSession("ENGL", "101");
-
         Student student1 = new Student("Cain DiVoe");
         session.enroll(student1);
         assertThat(session.getNumberOfStudents(), is(1));
-        ArrayList<Student> allStudents = session.getAllStudents();
-        assertThat(allStudents.size(), is(1));
-        assertThat(allStudents.get(0), is(student1));
+        assertThat(session.get(0), is(student1));
 
         Student student2 = new Student("Coralee DeVaughn");
         session.enroll(student2);
         assertThat(session.getNumberOfStudents(), is(2));
-        assertThat(allStudents.size(), is(2));
-        assertThat(allStudents.get(0), is(student1));
-        assertThat(allStudents.get(1), is(student2));
+        assertThat(session.get(0), is(student1));
+        assertThat(session.get(1), is(student2));
 
+    }
+
+    @Test
+    public void testCourseDates() throws Exception {
+        CourseSession session = new CourseSession("ABCD", "200", startDate);
+        Date sixteenWeeksOut = createDate(2003, 4, 25);
+        assertThat(session.getEndDate(), is(sixteenWeeksOut));
     }
 }
